4d6e81a8e39c3d4256bcca529fc46cf543540467
despean <despean15@gmail.com>
2017-05-03 20:55:03 +0100

    tagging functionality added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, get_object_or_404 , redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -17,7 +23,7 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -35,11 +41,17 @@ def post_detail(request, year, month, day, post):
             new_comment.post = post
             # Save the comment to the database
             new_comment.save()
-            return redirect(''+post.get_absolute_url())
+            return redirect('' + post.get_absolute_url())
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/details.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
-
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/details.html', {'post': post, 'comments': comments, 'comment_form': comment_form,
+                                                      'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -56,7 +68,7 @@ def post_share(request, post_id):
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            send_mail(subject, message, 'admin@myblog.co', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
