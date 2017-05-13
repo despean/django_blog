7892ee4a9948f5f2722b233a8558107c98c5c97e
despean <despean15@gmail.com>
2017-05-13 04:25:56 +0100

    Update settings.py

--- sblog/settings.py
+++ sblog/settings.py
@@ -123,7 +123,7 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "blog/static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "blog/static"),
 ]
